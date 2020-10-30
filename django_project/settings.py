eadbb1e83cf63ae7a059a45d545d733fd8655126
penny <fangle0121@gmail.com>
2020-10-29 17:01:20 -0700

    added ip address

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('mock_secrete_key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['167.172.158.115']
 
 
 # Application definition
