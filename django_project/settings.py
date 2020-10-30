851cb16ef7670ed4e8e4621a8c7cec752a968ad3
penny <fangle0121@gmail.com>
2020-10-29 17:31:12 -0700

    debug to be false

--- django_project/settings.py
+++ django_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('mock_secrete_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['167.172.158.115']
 
