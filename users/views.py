c1bd2a07067e11c8eea23c39999511a988572bff
jingyiluofd <jingyiluofd@gmail.com>
2020-11-18 02:12:02 -0800

    configuration for social login

--- users/views.py
+++ users/views.py
@@ -8,6 +8,9 @@ from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm, Programm
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+class HomeView(LoginRequiredMixin, TemplateView):
+    template_name = "home.html" 
+
 
 def register(request):
 	if request.method == 'POST':
