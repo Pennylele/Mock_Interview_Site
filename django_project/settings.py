48a73ce7fb6b1967a8fd59d104df08d08fc997c7
penny <fangle0121@gmail.com>
2020-11-19 15:31:08 -0800

    change the environment variable back

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'askdkajshdkjahd'
+SECRET_KEY = os.environ.get('mock_secrete_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
