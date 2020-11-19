aac68b091db6a7333c017b9b6be408c19175eff4
penny <fangle0121@gmail.com>
2020-11-19 11:13:34 -0800

    fix the deployment static_root issue

--- django_project/settings.py
+++ django_project/settings.py
@@ -177,8 +177,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
