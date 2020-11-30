1c14e0348c8a8b586919545b770b3959d97e1beb
penny <fangle0121@gmail.com>
2020-11-29 18:04:54 -0800

    changing global variables back

--- static/code-editor/Temp.java
+++ static/code-editor/Temp.java
@@ -1,12 +1,10 @@
+public class Temp {
+    public static void main(String[] args) {
+        System.out.println("Hello, java!");
+        System.out.println(add(111, 2));
+    }
 
-        public class Temp {
-            public static void main(String[] args) {
-                System.out.println("Hello, java!");
-                System.out.println(add(111, 2));
-            }
-
-            public static int add(int a, int b) {
-                return a + b;
-            }
-        }
-    
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
