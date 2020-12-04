146338f3d3204e69238a18913addcc49e4b4784f
penny <fangle0121@gmail.com>
2020-12-03 21:34:11 -0800

    removing all content from the temp files

--- mock_site/static/code-editor/temp.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-
-int main()
-{
-   cout << "hellocpp";
-   return 0;
-}
--- mock_site/static/code-editor/temp.py
+++ mock_site/static/code-editor/temp.py
@@ -1,10 +1,2 @@
-print(123)
-	print('abc')
 
-
-	def add(a, b):
-		return a + b
-
-
-	add(1, 2)
 	
