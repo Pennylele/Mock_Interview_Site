c63fea29c381621e5ce94e297786f21460a19ca3
penny <fangle0121@gmail.com>
2020-12-03 19:34:01 -0800

    making changes to the code-editor under static

---
+++ static/code-editor/Temp.java
@@ -0,0 +1,12 @@
+
+        public class Temp {
+            public static void main(String[] args) {
+                System.out.println("Hello, java!");
+                System.out.println(add(111, 2));
+            }
+
+            public static int add(int a, int b) {
+                return a + b;
+            }
+        }
+    
--- static/code-editor/temp.py
+++ static/code-editor/temp.py
@@ -1,8 +1,10 @@
 print(123)
-print('abc')
-
-def add(a, b):
-    return a + b
+	print('abc')
 
 
-print(add(1, 2))
+	def add(a, b):
+		return a + b
+
+
+	add(1, 2)
+	
