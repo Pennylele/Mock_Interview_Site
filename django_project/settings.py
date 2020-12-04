662bec058d28c05562c92f192f25d0def19de79e
penny <fangle0121@gmail.com>
2020-12-03 17:41:57 -0800

    changing back the  ip

--- django_project/settings.py
+++ django_project/settings.py
@@ -111,8 +111,8 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('themocksite', 6379)],
-            # "hosts": [('127.0.0.1', 6379)],
+            # "hosts": [('themocksite', 6379)],
+            "hosts": [('127.0.0.1', 6379)],
         },
     },
 }
