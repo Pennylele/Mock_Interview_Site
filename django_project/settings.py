c72a6637e4e896674d18dfcbc3b8ee895b3b823f
penny <fangle0121@gmail.com>
2020-12-03 22:19:29 -0800

    change port

--- django_project/settings.py
+++ django_project/settings.py
@@ -112,7 +112,7 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             # "hosts": [('themocksite', 6379)],
-            "hosts": [('167.172.158.115', 6379)],
+            "hosts": [('167.172.158.115', 5432)],
         },
     },
 }
