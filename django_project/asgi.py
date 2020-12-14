0113eec3163809e845fdf9ae7f036c96fbe2ded3
penny <fangle0121@gmail.com>
2020-12-13 23:14:34 -0800

    trying out another asgi line

--- django_project/asgi.py
+++ django_project/asgi.py
@@ -10,6 +10,7 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
+os.environ['DJANGO_SETTINGS_MODULE'] = "django_project.settings_production"
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 from channels.auth import AuthMiddlewareStack
