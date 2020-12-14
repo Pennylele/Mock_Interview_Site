01d5e9124d6fa48b38eac8540b0e1cb01abd6aa7
penny <fangle0121@gmail.com>
2020-12-13 23:27:29 -0800

    trying out another asgi version

--- django_project/asgi.py
+++ django_project/asgi.py
@@ -9,24 +9,25 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
 
 import os
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-os.environ['DJANGO_SETTINGS_MODULE'] = "settings"
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django.settings")
+# os.environ['DJANGO_SETTINGS_MODULE'] = "django.settings"
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 import mock_site.routing
-
-
+from channels.routing import get_default_application
 import django
+
+application = get_default_application()
 django.setup()
 
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            mock_site.routing.websocket_urlpatterns
-        )
-    ),
-})
+# application = ProtocolTypeRouter({
+#     "http": get_asgi_application(),
+#     "websocket": AuthMiddlewareStack(
+#         URLRouter(
+#             mock_site.routing.websocket_urlpatterns
+#         )
+#     ),
+# })
